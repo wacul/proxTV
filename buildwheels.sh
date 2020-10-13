9dd4ea0b4f9a9819944f8a3d9c0c09addb47a9ff
tomonoharuki <tomono@wacul.co.jp>
2020-10-13 19:10:43 +0900

    fix: build

--- buildwheels.sh
+++ buildwheels.sh
@@ -3,13 +3,7 @@
 # Must be run inside container quay.io/pypa/manylinux1_x86_64
 set -v
 
-# Check arguments
-if [ "$#" -ne 2 ]; then
-    echo "Parameters: <python version to use in build> <python packaging (m or mu)>"
-    exit
-fi
 pythonversion=$1
-packaging=$2
 
 # Install system packages required by the library
 yum install -y blas-devel lapack-devel
@@ -20,4 +14,4 @@ export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:/usr/lib64"
 "/opt/python/cp${pythonversion}-cp${pythonversion}${packaging}/bin/pip" wheel /io/ -w wheelhouse/
 
 # Bundle external shared libraries into the wheel
-auditwheel repair wheelhouse/prox_tv-*-cp${pythonversion}-cp${pythonversion}${packaging}-linux_x86_64.whl -w /io/wheelhouse/
+auditwheel repair wheelhouse/prox_tv-*-cp${pythonversion}-cp${pythonversion}-linux_x86_64.whl -w /io/wheelhouse/
